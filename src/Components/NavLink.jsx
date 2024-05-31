a8a897befec297aec10e215d10f5908522c4e364
Akhin <akhintheruvath2827@gmail.com>
2024-05-31 17:45:07 +0530

    creating NavLink component

---
+++ src/Components/NavLink.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NavLink = ({ to, active, children }) => {
+  return (
+    <Link
+      to={to}
+      className={`font-nunito font-semibold flex items-center h-11 mb-6 w-full cursor-pointer ${active ? 'text-[#605BFF] opacity-100 bg-slate-100' : 'text-[#030229] opacity-50'}`}
+    >
+      {children}
+    </Link>
+  );
+};
