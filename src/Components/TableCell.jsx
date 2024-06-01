f491f5db5df3cb94268480ec9136d9ece0378eef
Akhin <akhintheruvath2827@gmail.com>
2024-06-01 17:25:44 +0530

    removing min width

--- src/Components/TableCell.jsx
+++ src/Components/TableCell.jsx
@@ -1,6 +1,6 @@
 export const TableCell = ({ extraClasses = '', children }) => {
    return (
-      <td className={`border-[#F5F5F5] border-t-8 min-w-14 text-left text-sm text-[#231F20] px-5 py-4 ${extraClasses}`}>
+      <td className={`border-[#F5F5F5] border-t-8 text-left text-sm text-[#231F20] px-5 py-4 ${extraClasses}`}>
          {children}
       </td>
    );
